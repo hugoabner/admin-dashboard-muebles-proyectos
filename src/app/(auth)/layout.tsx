9d90910aa6a42db2fd08addf40ccce33808128ce
Hugo <hugopalominogalindo974@gmail.com>
2025-10-17 22:33:24 -0500

    agregar buton component

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+export default function LayoutAuth({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+      <nav
+        className="fixed top-0 flex items-center bg-background 
+        border-b h-16 w-full mx-auto justify-between px-4 shadow-xs"
+      >
+        <Button variant={"destructive"}>Como</Button>
+      </nav>
+        {children}
+    </main>
+  );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+
+export default function NotFound() {
+  return (
+    <div className="flex h-dvh flex-col items-center justify-center space-y-2 text-center">
+      <h1 className="text-2xl font-semibold">Página no encontrada.</h1>
+      <p className="text-muted-foreground">
+        No se pudo encontrar la página que estás buscando.
+      </p>
+      <Link replace href="/">
+        <Button variant="outline">Volver ala página principal</Button>
+      </Link>
+    </div>
+  );
+}
