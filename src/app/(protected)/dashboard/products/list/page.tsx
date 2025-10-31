6fb56ad5f3537aee42b12332bb48f763c4b36f2e
Hugo <hugopalominogalindo974@gmail.com>
2025-10-30 21:28:33 -0500

    prisma acelerate

--- src/app/(protected)/dashboard/products/list/page.tsx
+++ src/app/(protected)/dashboard/products/list/page.tsx
@@ -1,6 +1,54 @@
+const userData = [
+  {
+    id: 1,
+    name: "Alice",
+    email: "alice@prisma.io",
+    posts: {
+      create: [
+        {
+          title: "Join the Prisma Discord",
+          content: "https://pris.ly/discord",
+          published: true,
+        },
+        {
+          title: "Prisma on YouTube",
+          content: "https://pris.ly/youtube",
+        },
+      ],
+    },
+  },
+  {
+    id: 2,
+    name: "Bob",
+    email: "bob@prisma.io",
+    posts: {
+      create: [
+        {
+          title: "Follow Prisma on Twitter",
+          content: "https://www.twitter.com/prisma",
+          published: true,
+        },
+      ],
+    },
+  },
+];
 
 export default function page() {
   return (
-	<div>List</div>
-  )
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
+      <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
+        Posts
+      </h1>
+      <ul className="font-[family-name:var(--font-geist-sans)] max-w-2xl space-y-4">
+        {userData.map((post) => (
+          <li key={post.id}>
+            <span className="font-semibold">{post.name}</span>
+            <span className="text-sm text-gray-600 ml-2">
+              by {post.email}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
